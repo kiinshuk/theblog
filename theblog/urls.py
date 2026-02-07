c76100440dca959358f6d200facc3866c53d890c
kiinshuk <red2k2green@gmail.com>
2026-02-08 00:30:11 +0530

    error fixed

--- theblog/urls.py
+++ theblog/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/remove/<int:pk>', DeletePostView.as_view(), name="delete_post"),
     path('category/<int:cats>', CategoryView, name='category'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+
+
+
 ]
